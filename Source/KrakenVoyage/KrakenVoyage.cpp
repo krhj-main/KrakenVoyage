1c4c55254e583bd4f12f1c30585e5f5cb3b717e5
krhj-main <dygks6666@gmail.com>
2026-06-14 11:06:34 +0900

    chore: KrakenVoyage C++ 프로젝트 초기셋업 UE5.7)

---
+++ Source/KrakenVoyage/KrakenVoyage.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "KrakenVoyage.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, KrakenVoyage, "KrakenVoyage" );
